ff81c5ae65f752a9e337f3395c0e78f4329bd889
alexandermendes <alexanderhmendes@gmail.com>
2016-07-29 17:34:58 +0100

    Add Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,23 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        'gh-pages': {
+            options: {
+                base: 'dist'
+            },
+            src: ['**']
+        },
+        jshint: {
+            files: ['Gruntfile.js', 'dist/assets/js/*.js'],
+            options: {
+                globals: {
+                  jQuery: true
+                }
+            }
+        },
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-gh-pages');
+
+};
