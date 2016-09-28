1d8ab8ce42e1d7763d70af81c5b3ffd224cc3ed8
alexandermendes <alexanderhmendes@gmail.com>
2016-09-28 19:14:22 +0100

    Refactor installation

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,37 @@
 module.exports = function(grunt) {
 
     grunt.initConfig({
-        'gh-pages': {
-            options: {
-                base: 'dist'
-            },
-            src: ['**']
-        },
         jshint: {
-            files: ['Gruntfile.js', 'dist/assets/js/*.js'],
+            files: ['js/*.js'],
             options: {
                 globals: {
                   jQuery: true
                 }
             }
+        },
+        concat: {
+            js: {
+                src: [
+                    'node_modules/jquery/dist/jquery.min.js',
+                    'node_modules/bootstrap-select/dist/js/bootstrap-select.js',
+                    'node_modules/mustache/mustache.js',
+                    'node_modules/js-cookie/src/js.cookie.js',
+                    'node_modules/db.js/dist/db.min.js',
+                    'node_modules/showdown/dist/showdown.js',
+                    'node_modules/bootpag/lib/jquery.bootpag.js',
+                    'node_modules/codemirror/lib/codemirror.js',
+                    'node_modules/codemirror/mode/xml/xml.js',
+                    'assets/js/table.js',
+                    'assets/js/scripts.js',
+                ],
+                dest: 'js/dist/bundle.js'
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-gh-pages');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+
+    grunt.registerTask( 'default', ['jshint', 'concat']);
 
 };
