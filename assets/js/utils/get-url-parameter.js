9372445cdbf1ee360882f53f4f94c0f5bc6a6514
alexandermendes <alexanderhmendes@gmail.com>
2016-10-16 20:37:11 +0100

    Update msg

--- assets/js/utils/get-url-parameter.js
+++ assets/js/utils/get-url-parameter.js
@@ -6,12 +6,12 @@ var getUrlParameter = function(param, type) {
         query = URI.parseQuery(uri.query());
 
     if (!uri.hasQuery(param)) {
-        throw new Error(`Parameter ${param} missing from URL`);
+        throw new Error(`Parameter "${param}" missing from URL`);
     }
 
     if (type == 'int') {
         if (isNaN(parseInt(query[param]))) {
-            throw new Error(`Parameter ${param} must be an integer`);
+            throw new Error(`Parameter "${param}" must be an integer`);
         }
         return parseInt(query[param]);
     }
