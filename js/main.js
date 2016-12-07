5e6cb73d7ded3f7623ab512a3c703d394d15b7cb
alexandermendes <alexanderhmendes@gmail.com>
2016-12-07 18:49:58 +0000

    Remove animation js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 import editor from './view/editor';
-import table from './view/table';
+import tableView from './view/table';
 import upload from './view/upload';
 import notify from './view/notify';
 import docs from './view/docs';
 import landing from './view/landing';
+
 import checkHTML5 from './utils/check-html5';
 
 try {
@@ -14,20 +15,5 @@ try {
 }
 
 Pace.on('done', function() {
-    $("[data-animate-in]").each(function() {
-        $(this).addClass('animated ' + $(this).data('animate-in'));
-        $(this).removeClass('invisible');
-    });
     editor.refresh();
-});
-
-$('.animate-out').on('click', function(evt) {
-    var url = $(this).attr("href");
-    $("[data-animate-out]").each(function() {
-        $(this).addClass('animated ' + $(this).data('animate-out'));
-    });
-    setTimeout(function() {
-        window.location = url;
-    }, 1000);
-    evt.preventDefault();
 });
