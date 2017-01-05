bfd1241647e7fa0a35eedb8809f77ccb2124cb1b
alexandermendes <alexanderhmendes@gmail.com>
2017-01-05 16:36:06 +0000

    Update JSON export filename

--- _js/utils/export-json.js
+++ _js/utils/export-json.js
@@ -9,7 +9,7 @@ const exportJSON = function(records, xsltFilename) {
     const json = JSON.stringify(records.map(function(el) {
         return el[xsltFilename];
     }), null, 2);
-    zip.file('data.json', json);
+    zip.file(`${xsltFilename}-data.json`, json);
     zip.generateAsync({type:'blob'}).then(function(blob) {
         saveAs(blob, 'teiviewer-json-export.zip');
     });
