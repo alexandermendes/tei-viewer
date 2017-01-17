01ef03062e1cf2a5ca166e597f456c0e864fb95a
alexandermendes <alexanderhmendes@gmail.com>
2017-01-17 00:44:26 +0000

    Remove babel-transform-runtime

--- Gruntfile.js
+++ Gruntfile.js
@@ -52,8 +52,7 @@ module.exports = function(grunt) {
                             exclude: /node_modules/,
                             loader: 'babel-loader',
                             query: {
-                                presets: ['es2015'],
-                                plugins: ['transform-runtime']
+                                presets: ['es2015']
                             }
                         }
                     ]
